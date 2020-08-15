fe2e01a625360d624e4099236e991a916ffe199b
johnmontaguehowitt <jmhowitt@mac.com>
2020-08-15 21:40:38 +0100

    added a bit more Multi-line description of commit, feel free to be detailed.
    [Ticket: X]

--- misctwo.hsproj/misctwo/Main.hs
+++ misctwo.hsproj/misctwo/Main.hs
@@ -6,3 +6,20 @@ fac n =
     else
       n * fac (n-1)
       
+-- with guards.
+-- haskell will run through the guards in order and take
+-- the first true statement
+
+fac1 n
+  | n <= 1 = 1
+  | otherwise = n * fac1 (n-1)
+  
+-- otherwise always evaluates to true
+
+-- accumulators
+
+fac2 n = aux n 1
+  where
+    aux n acc
+      | n <= 1 = acc
+      | otherwise = aux (n-1) (n*acc)
